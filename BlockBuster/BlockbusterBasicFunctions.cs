a1b2a8fdff7cd3eaec9534592e22d6d2adeb3690
Swans311 <DSwansey@email.neit.edu>
2022-01-31 17:49:15 -0500

    Add project files.

---
+++ BlockBuster/BlockbusterBasicFunctions.cs
@@ -0,0 +1,132 @@
+﻿using BlockBuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBuster
+{
+    public class BlockbusterBasicFunctions
+    {
+        public static Movie GetMovieById(int id)
+        {
+            try
+            {
+                using(var context = new SE407_BlockBusterContext())
+                {
+                    return context.Movies.Find(id);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public static List<Movie> GetAllMovies()
+        {
+            try
+            {
+                using(var context = new SE407_BlockBusterContext())
+                {
+                    return context.Movies.ToList();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+
+        public static List<Movie> GetAllCheckedOutMovies()
+        {
+            using(var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies
+                    .Join(db.Transactions,
+                    m => m.MovieId,
+                    t => t.Movie.MovieId,
+                    (m, t) => new
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId,
+                        CheckedIn = t.CheckedIn
+                    }).Where(w => w.CheckedIn == "N")
+                    .Select(m => new Movie
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId
+                    }).ToList();
+            }
+        }
+
+
+        public static List<Movie> GetMoviesByGenreDescription(String Desc)
+        {
+            using (var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies
+                    .Join(db.Genres,
+                    m => m.GenreId,
+                    g => g.GenreId,
+                    (m, g) => new
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        GenreDescr = g.GenreDescr,
+                        DirectorId = m.DirectorId
+                    }).Where(g => g.GenreDescr == Desc)
+                    .Select(m => new Movie
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId
+                    }).ToList();
+            }
+        }
+
+        public static List<Movie> GetMoviesByDirectorLName(String LName)
+        {
+            using (var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies
+                    .Join(db.Directors,
+                    m => m.DirectorId,
+                    d => d.DirectorId,
+                    (m, d) => new
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        LastName = d.LastName,
+                        DirectorId = m.DirectorId
+                    }).Where(d => d.LastName == LName)
+                    .Select(m => new Movie
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId
+                    }).ToList();
+            }
+        }
+
+
+    }
+}
---
+++ BlockBusterTest/BlockBusterBasicFunctionsTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BlockBuster;
+
+namespace BlockBusterTest
+{
+    public class BlockBusterBasicFunctionsTest
+    {
+        [Fact]
+        public void GetMovieByIdTest()
+        {
+            var result = BlockbusterBasicFunctions.GetMovieById(11);
+            Assert.True(result.Title.ToLower() == "vertigo");
+            Assert.True(result.ReleaseYear == 1958);
+
+        }
+
+        [Fact]
+        public void GetAllMoviesTest()
+        {
+            var result = BlockbusterBasicFunctions.GetAllMovies();
+            Assert.True(result.Count == 49);
+        }
+
+        [Fact]
+        public void GetAllCheckedOutMovies()
+        {
+            var result = BlockbusterBasicFunctions.GetAllCheckedOutMovies();
+            Assert.True(result.Count == 3);
+        }
+
+        [Fact]
+        public void GetMoviesByGenreTest()
+        {
+            var result = BlockbusterBasicFunctions.GetMoviesByGenreDescription("Drama");
+            Assert.True(result.Count == 27);
+        }
+
+        [Fact]
+        public void GetMoviesByDirectorLNameTest()
+        {
+            var result = BlockbusterBasicFunctions.GetMoviesByDirectorLName("Eastwood");
+            Assert.True(result.Count == 1);
+        }
+
+    }
+}
