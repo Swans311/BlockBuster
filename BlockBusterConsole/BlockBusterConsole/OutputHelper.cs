4bb57770d372d2ac530fd3af0382dce926424900
Swans311 <59575829+Swans311@users.noreply.github.com>
2022-02-08 13:31:41 -0500

    Adding BlockBuster Console additions for Week4 lab

---
+++ BlockBusterConsole/BlockBusterConsole/OutputHelper.cs
@@ -0,0 +1,61 @@
+﻿using BlockBuster.Models;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBusterConsole
+{
+    class OutputHelper
+    {
+        public void WriteToConsole(List<Movie> movies)
+        {
+            Console.WriteLine("List of Movies");
+            foreach(var m in movies)
+            {
+                Console.WriteLine($"MovieID: {m.MovieId}     Title:{m.Title}     Release Year: {m.ReleaseYear}");
+            }
+        }
+
+        public void WriteToCSV(List<Movie> movies)
+        {
+            using (var writer = new StreamWriter("path\\to\\file.csv"))
+                using(var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(movies);
+            }
+        }
+
+        public void WriteMovies(List<Movie> movies, String input)
+        {
+            if (input.ToLower().Equals("csv"))
+            {
+                using (var writer = new StreamWriter(@"..\file.csv"))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(movies);
+                    Console.WriteLine("Movies Written into folder *insert folder location here*");
+                }
+            }
+            else if (input.ToLower().Equals("console"))
+            {
+                Console.WriteLine("List of Movies");
+                foreach (var m in movies)
+                {
+                    Console.WriteLine($"MovieID: {m.MovieId}     Title:{m.Title}     Release Year: {m.ReleaseYear}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please pick an option between: CSV or Console");
+                String userInput = Console.ReadLine();
+                WriteMovies(movies, userInput);
+            }
+        }
+
+    }
+}
---
+++ BlockBusterConsole/BlockBusterConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BlockBuster.Models;
+
+namespace BlockBusterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter CSV or Console for how you would like the movies returned to you.");
+            string userInput = Console.ReadLine();
+            var b = BlockBuster.BlockbusterBasicFunctions.GetAllMovies();
+            var oh = new OutputHelper();
+            //oh.WriteToCSV(b);
+            oh.WriteMovies(b, userInput);
+            Console.WriteLine("Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
