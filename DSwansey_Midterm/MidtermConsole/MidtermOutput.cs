90f2e12cd6034b2f787c1d95ae06561d500c1985
Swans311 <59575829+Swans311@users.noreply.github.com>
2022-02-08 19:18:14 -0500

    Added Midterm console

---
+++ DSwansey_Midterm/MidtermConsole/MidtermOutput.cs
@@ -0,0 +1,55 @@
+﻿using Midterm.Models;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermConsole
+{
+    class MidtermOutput
+    {
+        public void WriteToConsole(List<Book> books)
+        {
+            Console.WriteLine("List of Books");
+            foreach(var b in books)
+            {
+                Console.WriteLine($"BookID: {b.BookId}    Title: {b.BookTitle}");
+            }
+        }
+
+        public void WriteToCSV(List<Book> books)
+        {
+            using (var writer = new StreamWriter("path\\to\\file.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(books);
+            }
+        }
+
+        public void WriteBooks(List<Book> books, string input) //this is for the get by title option
+        {
+            if (input.ToLower().Equals("csv"))
+            {
+                WriteToCSV(books);
+                Console.WriteLine("Movies Written into folder *insert folder location here*");
+
+            }
+            else if (input.ToLower().Equals("console"))
+            {
+                //Console.WriteLine("List of Books");
+                WriteToConsole(books);
+            }
+            else
+            {
+                Console.WriteLine("Please pick an option between: CSV or Console");
+                String userInput = Console.ReadLine();
+                WriteBooks(books, userInput);
+            }
+        }
+
+    }
+}
---
+++ DSwansey_Midterm/MidtermConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Midterm.Models;
+
+namespace MidtermConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //content goes here...
+            Console.WriteLine("Hello Visitor, What can I do for you today?");
+            Console.WriteLine("Option 1: Get Book by Title.");
+            Console.WriteLine("Option 2: Get List of all Books we have.");
+            Console.WriteLine("Option 3: Get Books by a specific Author.");
+            Console.WriteLine("Please Enter 1,2,3 to select.");
+            var _input = Console.ReadLine();
+            var guestInput = Int32.Parse(_input);
+            while(guestInput>3 || guestInput < 1)
+            {
+                Console.WriteLine("Please Enter 1,2, or 3");
+                _input = Console.ReadLine();
+                guestInput = Int32.Parse(_input);
+
+
+            }
+            if (guestInput == 1)
+            {
+                Console.WriteLine("Enter the book title you want to search for...");
+                var input = Console.ReadLine();
+                var b = Midterm.MidtermFunctions.getBookByTitle(input);
+                Console.WriteLine("Do you want that as a CSV or printed to the console?");
+                var filetype = Console.ReadLine();
+                var mo = new MidtermOutput();
+                mo.WriteBooks(b, filetype);
+            }
+            else if (guestInput == 2)
+            {
+                Console.WriteLine("Searching for all Books...");
+                var input = Console.ReadLine();
+                var b = Midterm.MidtermFunctions.getAllBooks();
+                Console.WriteLine("Do you want that as a CSV or printed to the console?");
+                var filetype = Console.ReadLine();
+                var mo = new MidtermOutput();
+                mo.WriteBooks(b, filetype);
+            }
+            else if (guestInput == 3)
+            {
+                Console.WriteLine("Enter the Author's Last Name you want to search for...");
+                var input = Console.ReadLine();
+                var b = Midterm.MidtermFunctions.getBookByLName(input);
+                Console.WriteLine("Do you want that as a CSV or printed to the console?");
+                var filetype = Console.ReadLine();
+                var mo = new MidtermOutput();
+                mo.WriteBooks(b, filetype);
+            }
+            else
+            {
+
+            }
+            Console.WriteLine("Press Enter button to exit");
+            Console.ReadLine();
+        }
+    }
+}
